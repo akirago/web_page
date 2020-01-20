f5d12294a8480ca845434b0ded615de817297949
akirago <akirasyoku1234@gmail.com>
2020-01-20 13:20:19 +0900

    add TaskList View

---
+++ to-do/todo-app-native/App.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {
+  ActivityIndicator,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native'
+import TaskList from './src/TaskList';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <TaskList />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
---
+++ to-do/todo-app-native/src/TaskList.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import {
+  ActivityIndicator,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native'
+// functional conponent 
+const TaskList = () => {
+    return(
+        <View>
+            <Text>タスク一覧画面</Text>
+        </View>
+    );
+};
+
+export default TaskList;
